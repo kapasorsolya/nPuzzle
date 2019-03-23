c61369e6b726dab95c840266388ed2b52b2fd0d4
Kapas Orsolya <kapas.orsolya@yahoo.com>
2019-03-23 16:30:12 +0200

    template for the rand switch (part2)

--- nPuzzle/src/npuzzle/Program.java
+++ nPuzzle/src/npuzzle/Program.java
@@ -4,6 +4,11 @@
  * and open the template in the editor.
  */
 package npuzzle;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -83,7 +88,7 @@ public class Program {
                     } else {
                         int valueOfN = 0;
                         if (i + 1 == args.length - 1) {
-                            System.out.println("You should give N and M value");
+                            System.out.println("You should give M value too");
                             System.exit(0);
                         }
                         int valueOfM = 0;
